8018b65b08c31f4667cdfe221a5e44cb4f0de75c
Romel-cou <sromel001@gmail.com>
2020-07-17 20:31:17 +0600

    brickCalculator problem solved

--- assignment.js
+++ assignment.js
@@ -40,6 +40,28 @@ input = [6, 4, 4];
 var woodAmount = woodCalculator(input);
 console.log("Wood piece you will need(in cubic-feet):", woodAmount);
 
+// // ---------------->>brickCalculator<<----------------
 
+function brickCalculator(floorCount){
+    var bricks = 1000;
+    var bricksNeeded = floorCount * bricks;  
+    if(floorCount < 0){
+        return "You can't get result for negative input."; 
+    }
+    else if(floorCount <= 10){
+        var total1 = 15*bricksNeeded;
+        return total1;
+    }
+    else if(floorCount <= 20){
+        var total2 = 150000 + (floorCount - 10) * 12 * bricks;
+        return total2;
+    }
+    else{
+        var total3 = 270000 + (floorCount - 20) * 10 * bricks;
+        return total3;
+    }
+}
+var output = brickCalculator(45);
+console.log("Total number of bricks you will need:", output, "units"); 
 
 
